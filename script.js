443b8c51e0f84e254c9f77eb589a7b8f26163b57
harin <harin@qburst.com>
2020-03-10 11:45:15 +0530

    modified connect button behaviour

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let connect = $("#connect");
 let send = $("#send");
 let sent_btn = $("#sent_btn")
 let socket;
+let connectionFlag = 0
 
 sent_btn.attr("disabled", true);
 
@@ -9,14 +10,22 @@ connect.submit(function(event){
     sent_btn.attr("disabled", false);
     event.preventDefault()
     let ip = $("#ip").val();
-    if (ip.trim() != ""){
+
+    if (connectionFlag) {
+        socket.close()
+    }
+    if (connectionFlag === 0){
         $(".loader").css("display","block")
         let endpoint = 'ws://'+ip
         socket = new WebSocket(endpoint);
-
+        $('.connect-btn').prop('disabled', true);
         socket.onopen = function(e){
+            connectionFlag = 1
             $("#status").css("color","black")
             $("#status").html("Status: Connected")
+            $(".connect-btn").html("Disconnect")
+            $(".connect-btn").css("background-color","red")
+            $('.connect-btn').prop('disabled', false);
             $(".loader").css("display","none")
             console.log("open",e);
         }
@@ -34,9 +43,14 @@ connect.submit(function(event){
             if(e.wasClean){
                 $("#status").html("Status: Connection closed")
             }
+            connectionFlag = 0
+            $(".connect-btn").html("Connect")
+            $(".connect-btn").css("background-color","#007bff")
+            $('.connect-btn').prop('disabled', false);
             console.log("close",e);
         }
-    }else{
+    }
+    if (ip.trim() == ""){
         $("#status").css("color","red")
         $("#status").html("Status: End point is empty")
     }
