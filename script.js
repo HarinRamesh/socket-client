2cd88069b49f660021bdc0f86bc82bbe4efba674
harin <harin@qburst.com>
2020-03-10 11:25:09 +0530

    added loading bar

--- script.js
+++ script.js
@@ -8,25 +8,37 @@ sent_btn.attr("disabled", true);
 connect.submit(function(event){
     sent_btn.attr("disabled", false);
     event.preventDefault()
-
     let ip = $("#ip").val();
-    let endpoint = 'ws://'+ip
-    socket = new WebSocket(endpoint);
+    if (ip.trim() != ""){
+        $(".loader").css("display","block")
+        let endpoint = 'ws://'+ip
+        socket = new WebSocket(endpoint);
 
-    socket.onopen = function(e){
-        $("#status").html("Status: Connected")
-        console.log("open",e);
-    }
-    socket.onmessage = function(e){
-        console.log("message",JSON.parse(e.data));
-        $("#on-message").html(JSON.stringify(JSON.parse(e.data), undefined, 2))
-    }
-    socket.onerror = function(e){
-        $("#status").html("Status: Something went wrong")
-        console.log("error",JSON.stringify(e, undefined, 2));
-    }
-    socket.onclose = function(e){
-        console.log("close",JSON.stringify(e));
+        socket.onopen = function(e){
+            $("#status").css("color","black")
+            $("#status").html("Status: Connected")
+            $(".loader").css("display","none")
+            console.log("open",e);
+        }
+        socket.onmessage = function(e){
+            console.log("message",JSON.parse(e.data));
+            $("#on-message").html(JSON.stringify(JSON.parse(e.data), undefined, 2))
+        }
+        socket.onerror = function(e){
+            $(".loader").css("display","none")
+            $("#status").css("color","red")
+            $("#status").html("Status: Something went wrong,<br/> check the console.")
+            console.log("error",e);
+        }
+        socket.onclose = function(e){
+            if(e.wasClean){
+                $("#status").html("Status: Connection closed")
+            }
+            console.log("close",e);
+        }
+    }else{
+        $("#status").css("color","red")
+        $("#status").html("Status: End point is empty")
     }
 })
 
